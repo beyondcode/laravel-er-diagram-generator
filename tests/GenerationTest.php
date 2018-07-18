ef6c2a5c15808d0237324a1f1d0c1fdb1918c2ab
Jordan Hoff <jhoff@linkedin.com>
2018-07-18 12:00:14 -0500

    Adds tests for traits and default filenames

--- tests/GenerationTest.php
+++ tests/GenerationTest.php
@@ -46,4 +46,16 @@ class GenerationTest extends TestCase
 
         $this->assertMatchesSnapshot(Artisan::output());
     }
+
+    /** @test */
+    public function it_generated_graphviz_in_jpeg_format()
+    {
+        $this->app['config']->set('erd-generator.directories', [__DIR__.'/Models']);
+
+        Artisan::call('generate:erd', [
+            '--format' => 'jpeg'
+        ]);
+
+        $this->assertMatchesSnapshot(Artisan::output());
+    }
 }
--- tests/Models/Post.php
+++ tests/Models/Post.php
@@ -3,18 +3,15 @@
 namespace BeyondCode\ErdGenerator\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use BeyondCode\ErdGenerator\Tests\Traits\HasComments;
 
 class Post extends Model
 {
+    use HasComments;
 
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function comments()
-    {
-        return $this->hasMany(Comment::class);
     }
 
 }
