d027d5ea2fe24ffddf4652d8ca896a0bdac74e48
Daniel Werner <38726367+daniel-werner@users.noreply.github.com>
2019-10-14 08:40:35 +0200

    Fix the broken tests for older laravel versions (#63)
    * Sort the models to get the same results for laravel 5.6 as for the newer versions
    
    * Make the relations backward compatible with < laravel 5.8

--- src/RelationFinder.php
+++ src/RelationFinder.php
@@ -80,7 +80,7 @@ class RelationFinder
 
                 if ($return instanceof BelongsTo) {
                     $foreignKey = $this->getParentKey($return->getQualifiedOwnerKeyName());
-                    $localKey = $return->getForeignKeyName();
+                    $localKey = method_exists($return, 'getForeignKeyName') ? $return->getForeignKeyName() : $return->getForeignKey();
                 }
 
                 return [
