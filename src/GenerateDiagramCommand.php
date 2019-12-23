0c889eb1f631cbbc0826b1eead5452ff0ba78e0e
lizhuangzhuang <lizhuangzhuang@uniqueway.com>
2019-12-23 16:59:29 +0800

    Updated generate diagram command

--- src/GenerateDiagramCommand.php
+++ src/GenerateDiagramCommand.php
@@ -7,6 +7,8 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
 use phpDocumentor\GraphViz\Graph;
 use ReflectionClass;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class GenerateDiagramCommand extends Command
 {
@@ -15,11 +17,21 @@ class GenerateDiagramCommand extends Command
     const DEFAULT_FILENAME = 'graph';
 
     /**
+     * @var string
+     */
+    protected $modelNamespace = 'Modules\{#}\Entities\{#}';
+
+    /**
+     * @var string
+     */
+    protected $modulePath = 'Modules/{#}';
+
+    /**
      * The console command name.
      *
      * @var string
      */
-    protected $signature = 'generate:erd {filename?} {--format=png}';
+    protected $signature = 'generate:model-erd {models} {filename?} {--format=png}';
 
     /**
      * The console command description.
@@ -40,6 +52,13 @@ class GenerateDiagramCommand extends Command
     /** @var GraphBuilder */
     protected $graphBuilder;
 
+    /**
+     * GenerateDiagramCommand constructor.
+     *
+     * @param ModelFinder $modelFinder
+     * @param RelationFinder $relationFinder
+     * @param GraphBuilder $graphBuilder
+     */
     public function __construct(ModelFinder $modelFinder, RelationFinder $relationFinder, GraphBuilder $graphBuilder)
     {
         parent::__construct();
@@ -83,21 +102,19 @@ class GenerateDiagramCommand extends Command
         $this->info('Wrote diagram to ' . $this->getOutputFileName());
     }
 
+    /**
+     * @return string
+     */
     protected function getOutputFileName(): string
     {
         return $this->argument('filename') ?:
             static::DEFAULT_FILENAME . '.' . $this->option('format');
     }
 
-    protected function getModelsThatShouldBeInspected(): Collection
-    {
-        $directories = config('erd-generator.directories');
-
-        $modelsFromDirectories = $this->getAllModelsFromEachDirectory($directories);
-
-        return $modelsFromDirectories;
-    }
-
+    /**
+     * @param array $directories
+     * @return Collection
+     */
     protected function getAllModelsFromEachDirectory(array $directories): Collection
     {
         return collect($directories)
@@ -105,5 +122,50 @@ class GenerateDiagramCommand extends Command
                 return $this->modelFinder->getModelsInDirectory($directory)->all();
             })
             ->flatten();
+    }
+
+    /**
+     * @return Collection
+     */
+    protected function getModelsThatShouldBeInspected(): Collection
+    {
+        $models = explode(',', $this->argument('models'));
+
+        $modulePaths = [];
+        $modelNamespaces = [];
+        foreach ($models as $model) {
+            $path = explode('::', $model);
+            $modelNamespaces[] = Str::replaceArray('{#}', $path, $this->modelNamespace);
+            $modulePaths[] = Str::replaceArray('{#}', $path, $this->modulePath);
+        }
+
+        $directories = array_values(array_filter($modulePaths));
+        $modelsFromDirectories = $this->getAllModelsFromEachDirectory($directories)->intersect($modelNamespaces)
+            ->values();
+
+        foreach ($modelsFromDirectories as $modelsFromDirectory) {
+            $reflector = new \ReflectionClass($modelsFromDirectory);
+            $catFile = file_get_contents($reflector->getFileName());
+            preg_match_all('/(\w+)::class/', $catFile, $classRelations);
+            preg_match_all('/Modules\\\\(\w+)\\\\Entities\\\\(\w+)/', $catFile, $namespaceRelations);
+            $relationsFromNamespaces = Arr::last($namespaceRelations);
+            $namespaces = array_combine($relationsFromNamespaces, Arr::first($namespaceRelations));
+            $relations = Arr::last($classRelations);
+
+            foreach ($relations as $relation) {
+                if (! in_array($relation, $relationsFromNamespaces)) {
+                    $baseDir = substr($modelsFromDirectory, 0, strrpos($modelsFromDirectory, "\\"));
+                    $modelsFromDirectories->push($baseDir .'\\' . $relation);
+                }
+            }
+
+            foreach ($namespaces as $name => $namespace) {
+                if (in_array($name, $relations)) {
+                    $modelsFromDirectories->push($namespace);
+                }
+            }
+        }
+
+        return $modelsFromDirectories;
     }
 }
